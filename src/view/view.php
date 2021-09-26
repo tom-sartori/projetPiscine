6ae4157370ebb309b773ec3f20530c4fc6ac5957
Tom SARTORI <tom.sartori-letourneau@etu.umontpellier.fr>
2021-09-26 19:29:34 +0200

    Fix css link.

--- src/view/view.php
+++ src/view/view.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" href="./css/general.css">
+        <link rel="stylesheet" href="css/general.css">
         <meta charset="UTF-8">
         <title>
             <?= $pagetitle; ?>
