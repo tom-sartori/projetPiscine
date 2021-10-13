6fb779f17262b2521b11940fd531db5f7f1e6aa6
romain-mezghenna <romain.mezghenna@etu.umontpellier.fr>
2021-10-13 22:43:27 +0200

    ajout css

--- src/view/view.php
+++ src/view/view.php
@@ -4,44 +4,46 @@
 <head>
     <link rel="stylesheet" href="css/general.css">
     <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="css/recetteCreate.css">
     <script type="text/javascript" src="js/utils.js" defer></script>
     <meta charset="UTF-8">
     <title>
         <?= $pagetitle; ?>
     </title>
 </head>
-    <body>
-        <div class="menu">
-            <nav>
-                <ul>
-                    <li class="deroulant">
-                        <a href="index.php?controller=Recette&action=readAll" class="dropbtn">Recettes</a>
-                        <div class="deroule">
-                            <a href="index.php?controller=Recette&action=readAll">Voir la liste</a>
-                            <a href="index.php?controller=Recette&action=create">Ajouter une recette</a>
-                            <a href="index.php?controller=CategorieRecette&action=readAll">Catégories</a>
-                        </div>
-                    </li>
 
-                    <li class="deroulant">
-                        <a href="index.php?controller=Ingredient&action=readAll" class="dropbtn">Ingrédients</a>
-                        <div class="deroule">
-                            <a href="index.php?controller=Ingredient&action=readAll">Voir la liste</a>
-                            <a href="index.php?controller=Ingredient&action=create">Ajouter un ingrédient</a>
-                            <a href="index.php?controller=CategorieIngredient&action=readAll">Catégories</a>
-                        </div>
-                    </li>
+<body>
+    <div class="menu">
+        <nav>
+            <ul>
+                <li class="deroulant">
+                    <a href="index.php?controller=Recette&action=readAll" class="dropbtn">Recettes</a>
+                    <div class="deroule">
+                        <a href="index.php?controller=Recette&action=readAll">Voir la liste</a>
+                        <a href="index.php?controller=Recette&action=create">Ajouter une recette</a>
+                        <a href="index.php?controller=CategorieRecette&action=readAll">Catégories</a>
+                    </div>
+                </li>
 
-                    <li>
-                        <a href="index.php?controller=Allergene&action=readAll">Allergenes</a>
-                    </li>
+                <li class="deroulant">
+                    <a href="index.php?controller=Ingredient&action=readAll" class="dropbtn">Ingrédients</a>
+                    <div class="deroule">
+                        <a href="index.php?controller=Ingredient&action=readAll">Voir la liste</a>
+                        <a href="index.php?controller=Ingredient&action=create">Ajouter un ingrédient</a>
+                        <a href="index.php?controller=CategorieIngredient&action=readAll">Catégories</a>
+                    </div>
+                </li>
 
-                    <li>
-                        <a href="index.php?controller=Utilisateur&action=readAll">Utilisateur</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+                <li>
+                    <a href="index.php?controller=Allergene&action=readAll">Allergenes</a>
+                </li>
+
+                <li>
+                    <a href="index.php?controller=Utilisateur&action=readAll">Utilisateur</a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 
 
     <main>
